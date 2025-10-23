f8a85de2c63bed5a2a27a4329211ba4e603de727
Shubham Jaiswal <133669371+shubham042003@users.noreply.github.com>
2025-10-23 20:09:45 +0530

    Time: 8 ms (72.03%), Space: 44.9 MB (42.12%) - LeetHub

---
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+        while(s.length()>2){
+            StringBuilder next=new StringBuilder();
+            for(int i=0; i<s.length()-1; i++){
+                int sum=(s.charAt(i)-'0'+s.charAt(i+1)-'0')%10;
+                next.append(sum);
+            }
+            s=next.toString();
+        }
+        return s.charAt(0) == s.charAt(1);
+    }
+}
